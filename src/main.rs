b9a8a6c7ba4316391984d25fdd3ac17785fa64cc
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2026-03-16 17:53:07 +0800

    test: add unit test for demonstration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+fn greet(name: &'static str) -> String {
+    return format!("Hello, {}!", name);
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", greet("name"));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::greet;
+
+    #[test]
+    fn greet_with_name() {
+        let name = "name";
+        assert_eq!(greet(name), "Hello, name!");
+    }
 }
